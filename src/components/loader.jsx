941cc90c92d7a4e54c4ff045b534b826fa2a571b
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-08-12 16:28:49 +0100

    fixes: loader background issues

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -5,7 +5,7 @@ import ThemeContext from "../context/ThemeContext";
 export const Loader = () => {
   const { lightTheme } = useContext(ThemeContext);
   return (
-    <div className="fixed top-0 left-0 bottom-0 right-0 z-50 flex items-center justify-center">
+    <div className="fixed top-0 left-0 bottom-0 right-0 z-50 flex items-center justify-center bg-white">
       <CircleLoader color={lightTheme ? "#00859d" : "921267"} />
     </div>
   );
