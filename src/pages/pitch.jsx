d5eb150586cd42b1ccdf967bac35b43b55043285
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-08-13 19:07:41 +0100

    fix: replaces dummy elevator pitch with actual pitch

--- src/pages/pitch.jsx
+++ src/pages/pitch.jsx
@@ -9,49 +9,64 @@ const Pitch = () => {
 
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>Uzor Nwachukwu | Elevator Pitch</title>
       </Helmet>
-    <section className={`py-10`}>
-      <Reveal
-        variants={fadeInVariant}
-        className="flex items-center justify-center mx-auto"
-      >
-        <h1
-          className={`text-center text-2xl md:text-3xl py-2 px-10 font-bold border-b-4 ${
-            lightTheme ? "border-b-[#005269]" : "border-b-[#921267]"
-          } duration-300 w-fit mx-auto`}
+      <section className={`py-10`}>
+        <Reveal
+          variants={fadeInVariant}
+          className="flex items-center justify-center mx-auto"
         >
-          Personal Elevator Pitch
-        </h1>
-      </Reveal>
-      <div className="w-[90%] max-w-[1240px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 items-center py-5">
-        <Reveal>
-          <p className="leading-10 text-sm lg:text-base font-extralight">
-            Uzor Nwachukwu is a Frontend Developer at Providus Bank PLC, one of
-            Nigeria’s fastest growing commercial banks. He is passionate about
-            building responsive, scalable, testable and maintainable software
-            tools and solutions and hopes to become a senior software engineer
-            and a thought leader in the software engineering in the financial
-            sector in the next 5 years. Prior to joining Providus Bank, Uzor was
-            a software engineering fellow at The Bulb Africa, one of Nigeria
-            largest talent incubators and was awarded as the 3rd best fellow in
-            his cohort. He also holds a bachelor’s degree in mechanical
-            engineering from the Federal University of Petroleum resources
-            finishing in the top 15% of his class.
-          </p>
+          <h1
+            className={`text-center text-2xl md:text-3xl py-2 px-10 font-bold border-b-4 ${
+              lightTheme ? "border-b-[#005269]" : "border-b-[#921267]"
+            } duration-300 w-fit mx-auto`}
+          >
+            Personal Elevator Pitch
+          </h1>
         </Reveal>
-        <Reveal width="100%">
-          <iframe
-            className="w-full min-h-[50vh]"
-            src="https://www.youtube.com/embed/XhoTnkuD1vQ"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-          ></iframe>
-        </Reveal>
-      </div>
-    </section>
+        <div className="w-[90%] max-w-[1240px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 items-center py-5">
+          <Reveal>
+            <p className="leading-10 text-sm lg:text-base font-extralight text-justify">
+              Hello, My name is Uzor Nwachukwu, and I am a Software Engineer at
+              Providus Bank, one of Nigeria’s fastest-growing commercial banks.
+              <br />
+              I bring a unique blend of skills and experiences to the table.
+              With a solid engineering foundation from my studies in mechanical
+              engineering at the undergraduate level, coupled with a thriving
+              career in software engineering, I possess a diverse skill set that
+              revolves around problem-solving and analytical thinking.
+              <br /> My top three skills include exceptional problem-solving
+              abilities, strong analytical acumen, and a passion for continuous
+              learning. What sets me apart is my ability to bridge the gap
+              between engineering principles and cutting-edge software
+              solutions, making me an invaluable asset in various industries.{" "}
+              <br /> As your potential employee, I am committed to contributing
+              significant value to your organization. By leveraging my
+              interdisciplinary expertise, I can streamline processes, optimize
+              operations, and devise innovative solutions that will not only
+              make your jobs easier but also enhance overall efficiency and
+              productivity. <br /> Now, I kindly ask for your support and
+              consideration. If you or your company are seeking a dynamic and
+              versatile Software Engineer who can make a meaningful impact, I
+              would love the opportunity to discuss how my skills align with
+              your needs. My diverse background, problem-solving prowess, and
+              passion for innovation make me the ideal candidate to drive
+              positive change and deliver exceptional results. Thank you for
+              your time and attention.
+            </p>
+          </Reveal>
+          <Reveal width="100%">
+            <iframe
+              className="w-full min-h-[50vh]"
+              src="https://www.youtube.com/embed/XhoTnkuD1vQ"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </Reveal>
+        </div>
+      </section>
     </>
   );
 };
